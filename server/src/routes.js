7afe7317c30cbc3f97457385b5d54071d819d70c
Aplietexe <pietropalo@icloud.com>
2022-04-03 12:59:46 -0300

    Respond to /api/v1/temperatures with temperatures

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from "express"
 
+import { getTemperature, getTweetsByPool } from "./twitter/index.js"
+import getWbt from "./wbt.js"
+
 const router = Router()
 
-router.get("/temperatures", (_, res) => {
-  res.sendStatus(200)
+router.get("/temperatures", async (_, res) => {
+  const tweetsByPool = await getTweetsByPool()
+  const temperatures = tweetsByPool.map(({ pool, tweets }) => ({
+    pool,
+    temperature: getTemperature(tweets) ?? getWbt(),
+  }))
+
+  res.json(temperatures)
 })
 
 export default router
